0a166a1d700a01f31a9c7614c2ff80bf10d26873
iblue08 <adutta@aeonixinnovations.com>
2023-09-21 16:33:17 +0530

    implement job apply

--- web_client_ts/src/@types/interfaces/props/JobDetailsProps.ts
+++ web_client_ts/src/@types/interfaces/props/JobDetailsProps.ts
@@ -2,6 +2,7 @@ import { JobPostDetails } from "../../JobPostDetails";
 
 export interface JobDetailsProps{
     jobDetails:JobPostDetails,
+    isAccept?:boolean,
     InvitedJob?:string,
     jobId?:string,
     applicantId?:string,
