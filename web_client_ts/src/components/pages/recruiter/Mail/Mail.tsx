cae3ddcabbc041f423e47cb0c4ffc91660982db1
Tuhin Thakur <111550237+Tuhin810@users.noreply.github.com>
2023-10-03 16:22:30 +0530

    pwa service added

--- web_client_ts/src/components/pages/recruiter/Mail/Mail.tsx
+++ web_client_ts/src/components/pages/recruiter/Mail/Mail.tsx
@@ -2,7 +2,7 @@
 export const Mail = () => {
   return (
     <>
-    <button className="px-2 py-1 text-xs mx-3 rounded-full bg-blue-200 text-blue-600 border-1 hover:bg-blue-400 hover:text-blue-50 border-blue-400">Invite</button>
+    <button className="px-2 py-1 text-xs  rounded-full bg-blue-200 text-blue-600 border-1 hover:bg-blue-400 hover:text-blue-50 border-blue-400">Invite</button>
     </>
   )
 }
